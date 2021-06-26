b0e914264141869c2e89ff3c8d8b7dcde533a460
p_cxdan <cxdan@tinnove.com.cn>
2021-06-26 15:35:30 +0800

    tapd导航栏

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,88 +1,97 @@
 <template>
-  <div class="hello">
-    <el-menu
-      :default-active="activeIndex2"
-      class="el-menu-demo"
-      mode="horizontal"
-      @select="handleSelect"
-      background-color="#545c64"
-      text-color="#fff"
-      active-text-color="#ffd04b"
-    >
-      <el-menu-item index="1">处理中心</el-menu-item>
-      <el-submenu index="2">
-        <template slot="title">我的工作台</template>
-        <el-menu-item index="2-1">选项1</el-menu-item>
-        <el-menu-item index="2-2">选项2</el-menu-item>
-        <el-menu-item index="2-3">选项3</el-menu-item>
-        <el-submenu index="2-4">
-          <template slot="title">选项4</template>
-          <el-menu-item index="2-4-1">选项1</el-menu-item>
-          <el-menu-item index="2-4-2">选项2</el-menu-item>
-          <el-menu-item index="2-4-3">选项3</el-menu-item>
+  <el-container style="border: 1px solid #eee">
+    <el-aside style="background-color: rgb(255, 255, 255); width: 100px">
+      <i id="menuicon" class="el-icon-receiving" style="height: 5%"></i>
+      <el-menu :default-openeds="['1', '3']" collapse>
+        <el-menu-item index="1">
+          <i id="menuicon" class="el-icon-takeaway-box"></i>
+          <div>工作台</div>
+        </el-menu-item>
+        <el-menu-item index="2">
+          <i id="menuicon" class="el-icon-document"></i>
+          <div>文档</div>
+        </el-menu-item>
+        <el-menu-item index="3">
+          <i id="menuicon" class="el-icon-receiving"></i>
+          <div>项目</div>
+        </el-menu-item>
+        <el-submenu id="me" index="4">
+          <template slot="title">我</template>
+          <el-menu-item index="2-1">选项1</el-menu-item>
+          <el-menu-item index="2-2">选项2</el-menu-item>
+          <el-menu-item index="2-3">选项3</el-menu-item>
         </el-submenu>
-      </el-submenu>
-      <el-menu-item index="3">消息中心</el-menu-item>
-      <el-menu-item index="4"><a>订单管理</a></el-menu-item>
-      <el-submenu index="5" id="me">
-        <template slot="title">
-          <el-image
-            src="https://img2.baidu.com/it/u=2103035260,841148569&fm=26&fmt=auto&gp=0.jpg"
-            style="width: 50px; height: 50px"
-          ></el-image>
-          我
-        </template>
-        <el-menu-item index="5-1">选项1</el-menu-item>
-        <el-menu-item index="5-2">选项2</el-menu-item>
-        <el-menu-item index="5-3">选项3</el-menu-item>
-      </el-submenu>
-    </el-menu>
-  </div>
+      </el-menu>
+    </el-aside>
+
+    <el-container>
+      <el-header style="text-align: right; font-size: 12px">
+        <el-dropdown>
+          <i class="el-icon-setting" style="margin-right: 15px"></i>
+          <el-dropdown-menu slot="dropdown">
+            <el-dropdown-item>查看</el-dropdown-item>
+            <el-dropdown-item>新增</el-dropdown-item>
+            <el-dropdown-item>删除</el-dropdown-item>
+          </el-dropdown-menu>
+        </el-dropdown>
+        <span>王小虎</span>
+      </el-header>
+
+      <el-main>
+        <el-table :data="tableData">
+          <el-table-column prop="date" label="日期" width="140">
+          </el-table-column>
+          <el-table-column prop="name" label="姓名" width="120">
+          </el-table-column>
+          <el-table-column prop="address" label="地址"> </el-table-column>
+        </el-table>
+      </el-main>
+    </el-container>
+  </el-container>
 </template>
+
+<style>
+.el-header {
+  background-color: #193f72;
+  color: #333;
+  line-height: 60px;
+}
+
+.el-aside {
+  color: #333;
+}
+.el-menu-item,
+.el-submenu__title {
+  line-height: 30px;
+  margin-top: 10px;
+}
+.red_dan {
+  background-color: blue;
+}
+/* 不生效这个宽度，会引起宽度异常 */
+.el-menu--collapse {
+  width: unset;
+}
+#menuicon {
+  font-size: 30px;
+}
+#me {
+  position: absolute;
+  bottom: 0;
+}
+</style>
 
 <script>
 export default {
-  name: 'HelloWorld',
   data () {
-    return {
-      msg: 'Welcome to Your Vue.js App',
-      value1: 10,
-      value2: 50,
-      value3: 36,
-      value4: 48,
-      value5: 42,
-      activeIndex2: '1'
+    const item = {
+      date: '2016-05-02',
+      name: '王小虎',
+      address: '上海市普陀区金沙江路 1518 弄'
     }
-  },
-  methods: {
-    formatTooltip (val) {
-      return val / 100
-    },
-    handleSelect (val) {
-      console.log('select:' + val)
+    return {
+      tableData: Array(20).fill(item)
     }
   }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1,
-h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-#me {
-  float: right;
-}
-</style>
